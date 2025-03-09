6fb53db1d3c497f1b3dd5e96c084a9740bda7f7d
IvyCZE <147089690+IvyCZE@users.noreply.github.com>
2025-03-09 19:23:44 +0100

    cleanup login.dart

--- login.dart
+++ login.dart
@@ -11,7 +11,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,84 +30,19 @@ class _LoginScreenState extends State<LoginScreen> {
               Column(
                 children: [
                   Spacer(),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child:
-                    Text('Login or', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.1, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child:
-                    Text('Sign up', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.1, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start),
-                  ),
+                  _menuText('Register'),
+                  _menuText('or Log in'),
                   Spacer()
                 ],
               ),
               Column(
                 children: [
                   Spacer(),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: MediaQuery.of(context).size.width * 0.4256,
-                    height: MediaQuery.of(context).size.height * 0.2253,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(200))
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () => (),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 230, 160, 149),
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [SizedBox(width: 37), Icon(Icons.pattern, color: Color.fromARGB(200, 130, 100, 110), size: MediaQuery.of(context).size.width * 0.08,), Text('Google', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start)],
-                      ),
-                    ),
-                  ),
+                  _buildLoginButton("Google", Icons.pattern, 1),
                   Padding(padding: EdgeInsets.all(11),),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: MediaQuery.of(context).size.width * 0.4256,
-                    height: MediaQuery.of(context).size.height * 0.2253,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(200))
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () => (),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 230, 160, 149),
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [SizedBox(width: 37), Icon(Icons.key, color: Color.fromARGB(200, 130, 100, 110), size: MediaQuery.of(context).size.width * 0.08,), Text(' Other', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start)],
-                      ),
-                    ),
-                  ),
+                  _buildLoginButton("Local", Icons.password, 1),
                   Padding(padding: EdgeInsets.all(11),),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: MediaQuery.of(context).size.width * 0.4256,
-                    height: MediaQuery.of(context).size.height * 0.2253,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(200))
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () => (),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 230, 160, 149),
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [SizedBox(width: 45), Icon(Icons.password, color: Color.fromARGB(200, 130, 100, 110), size: MediaQuery.of(context).size.width * 0.08,), Text(' Local', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start)],
-                      ),
-                    ),
-                  ),
+                  _buildLoginButton("Return", Icons.arrow_back, 0.6),
                   Spacer()
                 ],
               ),
@@ -119,4 +53,108 @@ class _LoginScreenState extends State<LoginScreen> {
       )
     );
   }
+
+  Widget _menuText(String menuText) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child:
+      Text(menuText, style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.1, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start),
+    );
+  }
+
+  Widget _buildLoginButton(String loginText, IconData loginIcon, double size) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width * 0.4256 * size,
+      height: MediaQuery.of(context).size.height * 0.2253 * size,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(200))
+      ),
+      child: ElevatedButton(
+        onPressed: () {if(loginText == "Local") {
+            _localDialog();
+          } else if(loginText == "Return") {
+            Navigator.pop(context);
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 230, 160, 149),
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SizedBox(width: 37), Icon(loginIcon, color: Color.fromARGB(200, 130, 100, 110), size: MediaQuery.of(context).size.width * 0.08 * size,), Text(loginText, style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05 * size, fontWeight: FontWeight.bold, color: Color.fromARGB(200, 130, 100, 110)), textAlign: TextAlign.start)],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String hintText) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.3,
+      height: MediaQuery.of(context).size.height * 0.07,
+      padding: EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 230, 160, 149),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        expands: false,
+        maxLines: 1,
+        minLines: 1,
+        textAlignVertical: TextAlignVertical.top,
+        style: TextStyle(color: Color.fromARGB(200, 130, 100, 110), fontSize: MediaQuery.of(context).size.height * 0.025),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintStyle: TextStyle(color: Color.fromARGB(150, 130, 100, 130), fontSize: MediaQuery.of(context).size.height * 0.025),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonAction(String buttonText) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 230, 160, 149),
+      ),
+      onPressed: () {},
+      child: Text(buttonText, style: TextStyle(color: Color.fromARGB(255, 130, 100, 130), fontStyle: FontStyle.italic ,fontWeight: FontWeight.bold,),),
+    );
+  }
+
+  void _localDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color.fromARGB(255, 255, 230, 222),
+          title: const Text('Local Login', style: TextStyle(color: Color.fromARGB(255, 130, 100, 130), fontStyle: FontStyle.normal ,fontWeight: FontWeight.bold,),),
+          content:
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20,),
+                    _buildTextField("Username",),
+                    SizedBox(height: 20,),
+                    _buildTextField("Password",),
+                    SizedBox(height: 8,),
+                    _buildTextField("Pass again",),
+                  ],
+                ),
+              ),
+          actions: [
+            _buttonAction("Register Instead"),
+            _buttonAction("Log In")
+          ],
+        );
+      },
+    );
+  }
+
 }
